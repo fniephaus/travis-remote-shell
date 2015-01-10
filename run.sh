7ddbaf408985e3bd3e9dff151ff440cd2e22fb94
Fabio Niephaus <fniephaus@users.noreply.github.com>
2015-01-10 01:57:53 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install netcat
+wget -O ngrok.zip https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip\?os\=linux\&arch\=386\&channel\=stable
+unzip ngrok.zip
+{
+    nc.traditional -l -v -p 8888 -e /bin/bash
+    killall -SIGINT ngrok && echo "ngrok terminated"
+} &
+{
+    ./ngrok -authtoken="eXZTxAWPSFIr3TsBQCK6" -log="stdout" -proto=tcp 8888 | grep "Tunnel established at" || true
+} &
