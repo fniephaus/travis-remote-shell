6410dae23828ee8afaf7439dc5b48b04d79345e1
Fabio Niephaus <code@fniephaus.com>
2015-07-08 15:31:17 +0200

    Update to ngrok 2

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo apt-get install netcat
-wget -O ngrok.zip https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip\?os\=linux\&arch\=386\&channel\=stable
+wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
 unzip ngrok.zip
 {
     nc.traditional -l -v -p 8888 -e /bin/bash
     killall -SIGINT ngrok && echo "ngrok terminated"
 } &
 {
-    ./ngrok -authtoken=$NGROK_TOKEN -log="stdout" -proto=tcp 8888 | grep "Tunnel established at" || true
+    ./ngrok tcp 8888 --authtoken=$NGROK_TOKEN --log=stdout --log-level=debug | grep "tcp.ngrok.io" || true
 } &
