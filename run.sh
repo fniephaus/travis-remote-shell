5dab5bb3687bb24777935a8283bce2b782aec854
Fabio Niephaus <code@fniephaus.com>
2015-07-17 13:21:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
 unzip ngrok.zip
 {
-    nc.traditional -l -v -p 8888 -e /bin/bash
+    nc -l -v -p 8888 -e /bin/bash
     killall -SIGINT ngrok && echo "ngrok terminated"
 } &
 {
